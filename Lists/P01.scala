7daee669a2bbec69def063fa8eb674d92bdcc1e9
Dan Clark <dan.clark@quanttus.com>
2014-08-30 09:57:18 -0400

    P02

--- Lists/P01.scala
+++ Lists/P01.scala
@@ -1,13 +1,9 @@
 object P01 {
   def lastBuiltin[A](ls: List[A]): A = ls.last
   
-   println(lastBuiltin(List(1,2,3,4)))
-  
   def lastRecursive[A](ls: List[A]): A = ls match {
     case h :: Nil => h
     case _ :: tail => lastRecursive(tail)
     case _         => throw new NoSuchElementException
   }
-  
-  println(lastRecursive(List(1,2,3,4,5)))
 }
---
+++ Lists/P02.scala
@@ -0,0 +1,30 @@
+object P02 {
+  def secondLastBuiltin[A](ls: List[A]): A = {
+	if (ls.isEmpty) throw new NoSuchElementException
+	else ls.init.last
+  }
+  
+  
+	
+  def secondLastRecursive[A](ls: List[A]): A = ls match {
+    case head :: _ :: Nil => {
+      println(head)
+      head
+    }
+    case _ :: tail 	   => { 
+      println(tail)
+      secondLastRecursive(tail)
+    }
+    case _ 			   => throw new NoSuchElementException
+  }
+  
+ 
+  
+  //Generic Solution
+  def lastNthBuiltin[A](n: Int, ls: List[A]): A = {
+    if (n <= 0) throw new IllegalArgumentException
+    if (ls.length < n) throw new NoSuchElementException
+    ls.takeRight(n).head
+  } 
+}
+
